8bc345a480757f81a2e189108463ca58b97dff37
prach <prach@LAPTOP-JTQFL6K2>
2022-03-05 18:55:57 +0530

    new..

---
+++ javaprogram/src/TestingSomething/TryingNew.java
@@ -0,0 +1,21 @@
+package TestingSomething;
+
+public class TryingNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 5;
+		float b = 6.4f;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("a+b = "+(a+b));
+		System.out.println("a-b = "+(a-b));
+		System.out.println("a*b = "+(a*b));
+		System.out.println("a/b = "+(a/b));
+		System.out.println("a%b = "+(a%+b));
+		
+
+	}
+
+}
